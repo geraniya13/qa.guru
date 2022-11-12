aca64cfa424fb50229f577722df4b31b7ac88a95
geraniya13 <geranas13@gmail.com>
2022-11-12 19:22:25 +0100

    12th lesson homework test

--- src/test/java/demoqaProps/TestBase.java
+++ src/test/java/demoqaProps/TestBase.java
@@ -15,11 +15,18 @@ public class TestBase {
     RegistrationFormPage registrationFormPage = new RegistrationFormPage();
     Faker faker = new Faker();
     TestData testData = new TestData();
+    static String browserName = System.getProperty("browserName", "chrome");
+    static String browserVersion = System.getProperty("browserVersion", "100.0");
+    static String browserSize = System.getProperty("browserSize", "1920x1080");
     static String remoteUrl = System.getProperty("remoteUrl");
 
     @BeforeAll
     static void configure() {
         SelenideLogger.addListener("allureSelenide", new AllureSelenide());
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browser = browserName;
+        Configuration.browserVersion = browserVersion;
+        Configuration.browserSize = browserSize;
         if (remoteUrl != null) {
             DesiredCapabilities dc = new DesiredCapabilities();
             dc.setCapability("enableVNC", true);
@@ -27,11 +34,6 @@ public class TestBase {
             Configuration.browserCapabilities = dc;
             Configuration.remote = remoteUrl;
         }
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browser = System.getProperty("browserName", "chrome");
-        Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
-        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
-
     }
 
     @BeforeEach
